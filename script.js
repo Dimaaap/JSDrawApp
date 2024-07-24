40f3aa1268acd86eab7cd1e8d23ef3dcc54d38cb
Dimaaap <procdima49@gmail.com>
2024-07-24 23:15:31 +0300

    Add some draw logic

--- script.js
+++ script.js
@@ -27,9 +27,29 @@ ctx.fillStyle = "white";
 ctx.fillRect(stageProperties.left, stageProperties.right, 
     stageProperties.width, stageProperties.height);
  
+const path = [];
+
+
+
 myCanvas.addEventListener("pointerdown", (e) => {
     const mousePosition = {
         x: e.offsetX,
         y: e.offsetY
-    }
+    };
+    path.push(mousePosition);
+    myCanvas.addEventListener("pointermove", (e) => {
+        const mousePosition = {
+            x: e.offsetX,
+            y: e.offsetY
+        }
+        path.push(mousePosition);
+    })
+    myCanvas.addEventListener("pointerup", (e) => {
+        ctx.beginPath();
+        ctx.moveTo(path[0].x, path[0].y);
+        path.forEach((point) => {
+            ctx.lineTo(point.x, point.y);
+        });
+        ctx.stroke();
+    })
 })
