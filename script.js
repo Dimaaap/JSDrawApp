42bcec38f43abb5d04e9c1111a2a90ce9ce21ff0
Dimaaap <procdima49@gmail.com>
2024-09-01 19:18:10 +0300

    Reload code

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const myCanvas = document.getElementById("myCanvas");
-
 const canvasProperties = {
     width: window.innerWidth,
     height: window.innerHeight,
@@ -9,51 +7,48 @@ const canvasProperties = {
     }
 }
 
-myCanvas.height = canvasProperties.height;
-myCanvas.width = canvasProperties.width;
-
 const stageProperties = {
     width: 600,
-    height: 480,
-    left: canvasProperties.center.x - 600 / 2,
-    right: canvasProperties.center.y - 480 / 2
+    height: 480, 
+    left: canvasProperties.center.x-600/2,
+    top: canvasProperties.center.y-480/2
 }
 
+
+myCanvas.width = canvasProperties.width;
+myCanvas.height = canvasProperties.height;
+
 const ctx = myCanvas.getContext("2d");
+
 clearCanvas();
 
-
- 
 const shapes = [];
 let path = [];
-let rectangle = {};
-
 
 myCanvas.addEventListener("pointerdown", (e) => {
     const mousePosition = {
         x: e.offsetX,
         y: e.offsetY
     };
-    rectangle.corner1 = mousePosition
+    path.push(mousePosition)
 
     const moveCallback = (e) => {
         const mousePosition = {
             x: e.offsetX,
-            y: e.offsetY
-        };
-        rectangle.corner2 = mousePosition;
+            y: e.offsetY,
+        }
+        path.push(mousePosition)
 
         clearCanvas();
-        for(const shape of [...shapes, rectangle]){
+
+        for(const shape of [...shapes, path] ){
             ctx.beginPath();
-            ctx.strokeStyle="rgba(0, 0, 0, 0.5)";
+            ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
             ctx.lineWidth = 5;
-            const rect = shape;
-            const minX = Math.min(rect.corner1.x, rect.corner2.x);
-            const minY = Math.min(rect.corner1.y, rect.corner2.y);
-            const width = Math.abs(rect.corner1.x - rect.corner2.x);
-            const height = Math.abs(rect.corner1.y - rect.corner2.y);
-            ctx.rect(minX, minY, width, height)
+            ctx.moveTo(shape[0].x, shape[0].y);
+            for(let i = 1; i < shape.length; i++){
+                ctx.lineTo(shape[i].x, shape[i].y);
+            }
             ctx.stroke();
         }
     }
@@ -61,23 +56,27 @@ myCanvas.addEventListener("pointerdown", (e) => {
     const upCallback = e => {
         myCanvas.removeEventListener("pointermove", moveCallback);
         myCanvas.removeEventListener("pointerup", upCallback);
-        
-        shapes.push(rectangle);
-        rectangle = {};
+
+        shapes.push(path);
+        path = [];
+
 
     }
-
     myCanvas.addEventListener("pointermove", moveCallback)
-    myCanvas.addEventListener("pointerup", upCallback)
-})
-
+    myCanvas.addEventListener("pointerup", upCallback);
+});
 
 function clearCanvas(){
     ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+   
     ctx.fillStyle = "gray";
     ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
 
     ctx.fillStyle = "white";
-    ctx.fillRect(stageProperties.left, stageProperties.right, 
-        stageProperties.width, stageProperties.height);
+    ctx.fillRect(
+        stageProperties.left, 
+        stageProperties.top, 
+        stageProperties.width, 
+        stageProperties.height
+    )
 }
